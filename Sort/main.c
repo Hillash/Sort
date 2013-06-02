fc49ded3aed0009a07722504590a8782e581a79c
Ntrm <m0rt3l.king+github@gmail.com>
2013-06-02 16:38:25 +0200

    Initial Commit

---
+++ Sort/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Sort
+//
+//  Created by Martin on 02.06.13.
+//  Copyright (c) 2013 Ntrm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
